fbebcc1f28708c906e7c078fb48da69029a29051
Sondos-Al-alami <sondos.al.alami@monstar-lab.com>
2022-11-08 08:58:00 +0200

    refactor: import messages from files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+import en from "./locales/en-US.json";
+import ar from "./locales/ar.json";
+import bn from "./locales/bn-BD.json";
 
 export default defineNuxtConfig({
   meta: {
@@ -44,9 +47,9 @@ export default defineNuxtConfig({
   },
   i18n: {
     locales: [
-      { code: "en", name: "English", iso: "en-US", file: "en-US.js" },
-      { code: "ar", name: "Arabic", iso: "ar", file: "ar.js" },
-      { code: "bn", name: "Bangla", iso: "bn-BD", file: "bn-BD.js" },
+      { code: "en", name: "English", iso: "en-US", file: "en-US.json" },
+      { code: "ar", name: "Arabic", iso: "ar", file: "ar.json" },
+      { code: "bn", name: "Bangla", iso: "bn-BD", file: "bn-BD.json" },
     ],
     defaultLocale: "en",
     strategy: "prefix_except_default",
@@ -54,59 +57,7 @@ export default defineNuxtConfig({
       legacy: false,
       locale: "en",
       fallbackLocale: "en",
-      messages: {
-        en: {
-          links: {
-            about: "About",
-            home: "Home",
-            users: "Users",
-            profile: "Profile",
-            logout: "Logout",
-            register: "Register",
-            login: "Login",
-          },
-          pageTitle: {
-            home: "Home Page",
-            about: "About Page",
-            users: "All Users",
-            profile: "Profile Page",
-          },
-        },
-        ar: {
-          links: {
-            about: "من نحن",
-            home: "الرئيسية",
-            users: "المستخدمين",
-            profile: "ملف التعريف",
-            login: "تسجيل الدخول",
-            logout: "تسجيل الخروج",
-            register: "تسجيل الاشتراك",
-          },
-          pageTitle: {
-            home: "الصفحة الرئيسية",
-            about: "صفحة من نحن",
-            users: "قائمة المستخدمين",
-            profile: "صفحة ملف التعريف",
-          },
-        },
-        bn: {
-          links: {
-            about: "আমাদের সম্পর্কে",
-            home: "হোম পেজ",
-            users: "ব্যবহারকারীদের",
-            profile: "প্রোফাইল",
-            login: "প্রবেশ করুন",
-            logout: "প্রস্থান",
-            register: "নিবন্ধন",
-          },
-          pageTitle: {
-            home: "হোম পেজ",
-            about: "আমাদের সম্পর্কে পাতা",
-            users: "সকল ব্যবহারকারী",
-            profile: "প্রোফাইল পাতা",
-          },
-        },
-      },
+      messages: { en, ar, bn },
     },
   },
 });
