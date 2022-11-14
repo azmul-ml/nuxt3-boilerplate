e147c9eaf665ff99ce5e98d7b3efec99abd45686
maruf-ml <maruf.ahmed@monstar-lab.com>
2022-11-14 12:39:46 +0600

    feat: added chartsjs Added few examples

--- components/Header.vue
+++ components/Header.vue
@@ -5,7 +5,7 @@ const { logout } = useAuthStore();
 const { isLoggedIn } = storeToRefs(useAuthStore());
 
 const handleLogout = async () => {
-  logout();
+  await logout();
 };
 </script>
 
@@ -15,6 +15,7 @@ const handleLogout = async () => {
     <NuxtLink v-if="isLoggedIn" to="/user" class="pr-8">Users</NuxtLink>
     <NuxtLink v-if="isLoggedIn" to="/about" class="pr-8">About</NuxtLink>
     <NuxtLink v-if="isLoggedIn" to="/profile" class="pr-8">Profile</NuxtLink>
+    <NuxtLink v-if="isLoggedIn" to="/chart" class="pr-8">Charts</NuxtLink>
     <NuxtLink v-if="!isLoggedIn" to="/login" class="pr-8">Login</NuxtLink>
     <NuxtLink v-if="!isLoggedIn" to="/register" class="pr-8">Register</NuxtLink>
     <button v-if="isLoggedIn" class="pr-8" @click="handleLogout">Logout</button>
