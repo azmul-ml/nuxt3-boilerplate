7a55eece5320d0784b33b266ca145b56e0c2fdf9
azmul-ml <azmul.hossain@.monstar-lab.com>
2022-11-16 12:36:45 +0600

    feat: about and profile page layout updated

--- pages/about.vue
+++ pages/about.vue
@@ -13,16 +13,8 @@ useHead({
     <h3>{{ $t("pageTitle.about") }}</h3>
 
     <div class="bg-light rounded h-100 p-4">
-      <h6 class="mb-4">Testimonial</h6>
       <div class="owl-carousel testimonial-carousel">
         <div class="testimonial-item text-center">
-          <img
-            class="img-fluid rounded-circle mx-auto mb-4"
-            src="@/assets/images/user.png"
-            style="width: 100px; height: 100px"
-          />
-          <h5 class="mb-1">Azmul Hossain</h5>
-          <p>Software Engineer</p>
           <p class="mb-0">
             Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam
           </p>
