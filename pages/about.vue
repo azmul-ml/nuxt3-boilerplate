5677e58c6536f620e5b3f1785a9bf2e558750425
azmul-ml <azmul.hossain@.monstar-lab.com>
2022-11-16 15:47:40 +0600

    feat: loader added

--- pages/about.vue
+++ pages/about.vue
@@ -10,14 +10,10 @@ useHead({
 
 <template>
   <div>
-    <h3>{{ $t("pageTitle.about") }}</h3>
-
-    <div class="bg-light rounded h-100 p-4">
-      <div class="owl-carousel testimonial-carousel">
-        <div class="testimonial-item text-center">
-          <p class="mb-0">
-            Dolor et eos labore, stet justo sed est sed. Diam sed sed dolor stet amet eirmod eos labore diam
-          </p>
+    <div class="container-fluid pt-4 px-4">
+      <div class="row vh-100 bg-light rounded align-items-center justify-content-center mx-0">
+        <div class="col-md-6 text-center">
+          <h3>{{ $t("pageTitle.about") }}</h3>
         </div>
       </div>
     </div>
--- pages/user/index.vue
+++ pages/user/index.vue
@@ -22,11 +22,15 @@ fetchUsers();
     <div class="container-fluid pt-4 px-4">
       <div class="bg-light text-center rounded p-4">
         <div class="d-flex align-items-center justify-content-between mb-4">
-          <h6 class="mb-0">{{ $t("pageTitle.users") }}</h6>
+          <h3 class="mb-0">{{ $t("pageTitle.users") }}</h3>
           <button type="button" class="btn btn-primary" @click="navigateTo('/user/create')">Create User</button>
         </div>
         <div class="table-responsive">
-          <table class="table text-start align-middle table-bordered table-hover mb-0">
+          <Loader :is-loader="loading" />
+          <table
+            v-if="userState.users.length > 0"
+            class="table text-start align-middle table-bordered table-hover mb-0"
+          >
             <thead>
               <tr class="text-dark">
                 <th scope="col"><input class="form-check-input" type="checkbox" /></th>
