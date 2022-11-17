ea4aadcc9b437a3aa4ef0f6e02141572bdad15a1
azmul-ml <azmul.hossain@.monstar-lab.com>
2022-11-17 15:22:47 +0600

    feat: increased login/ register width

--- pages/register.vue
+++ pages/register.vue
@@ -46,7 +46,7 @@ const v$ = useVuelidate(rules, registerForm);
       <!-- Sign Up Start -->
       <div class="container-fluid">
         <div class="row h-100 align-items-center justify-content-center" style="min-height: 100vh">
-          <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+          <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-5">
             <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
               <div class="d-flex align-items-center justify-content-between mb-3">
                 <Anchor to="/">
