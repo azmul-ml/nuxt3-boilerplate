71179ed52acea27f8bcd5cdcb2c91ef813e35428
arunkundu <arun.kundu@monstarlab.com>
2022-11-18 01:45:33 +0600

    feat: api error handling

---
+++ components/error/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+const props = defineProps({
+  showDefaultError: { type: Boolean, required: true },
+});
+</script>
+
+<template>
+  <div>
+    <slot></slot>
+    <div v-if="props.showDefaultError" class="text-danger">Default error message: Something went wrong</div>
+  </div>
+</template>
